a9f2811c641c3bf5bc1491037b3ade4ce11538c9
Chico204 <olatunjimark391@gmail.com>
2025-06-02 14:31:48 +0100

    second

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,10 +5,47 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
- 
+  const displaySubmenu = (e)=> {
+    const page = e.target.textContent
+    const tempBtn = e.target.getBoundingClientRect()
+    const center = (tempBtn.left + tempBtn.right)/ 2
+    const bottom = tempBtn.bottom - 3
+    openSubmenu(page, {center, bottom})
+  }
+  const handleSubmenu = (e)=> {
+  if (!e.target.classList.contains('link-btn')){
+    closeSubmenu()
+  }
+
+  }
   return (
-    <nav className='nav' >
-    nav
+    <nav className='nav flex items-center justify-center bg-transparent relative z-[1]' onMouseOver={handleSubmenu} >
+    <div className="nav-center w-[90vw] md:grid md:grid-cols-[auto_1fr_auto] md:items-center">
+      <div className="nav-header flex justify-between items-center">
+        <h1>Stripe</h1>
+        <button className='btn toggle-btn md:hidden' onClick={openSidebar}>
+          <FaBars/>
+        </button>
+      </div>
+      <ul className="nav-links hidden md:grid md:grid-cols-3 md:text-center md:h-full md:items-center md:justify-self-center ">
+        <li className='h-full'>
+          <button className="link-btn h-full bg-transparent border-transparent text-[1.1rem] text-black capitalize tracking-wide w-40" onMouseOver={displaySubmenu}>
+            products
+          </button>
+           </li>
+            <li className='h-full'>
+          <button className="link-btn h-full bg-transparent border-transparent text-[1.1rem] text-black capitalize tracking-wide w-40" onMouseOver={displaySubmenu}>
+           developers
+          </button>
+           </li>
+            <li className='h-full'>
+          <button className="link-btn h-full bg-transparent border-transparent text-[1.1rem] text-black capitalize tracking-wide w-40" onMouseOver={displaySubmenu}>
+       company
+          </button>
+           </li>
+      </ul>
+      <button className='btn signin-btn bg-white text-xl hidden md:inline-block '>sign in</button>
+    </div>
     </nav>
   );
 };
